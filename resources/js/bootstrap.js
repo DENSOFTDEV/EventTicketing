7f2cc58f80ce7f85145cb22cd749723b9ff4154c
dennis githinji <dgithinji@techchimp.co.ke>
2021-11-19 02:08:28 +0300

    changes

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,7 +22,7 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = "https://laugh-show.herokuapp.com";
+window.axios.defaults.baseURL = "http://laugh-show.herokuapp.com";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
